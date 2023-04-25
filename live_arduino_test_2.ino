cfb6b5843488e2f7957a2fed3cbc34373d81b1ef
penenadpi <penenadpi@gmail.com>
2023-04-25 20:31:09 +0200

    Add files via upload

--- live_arduino_test_2.ino
+++ live_arduino_test_2.ino
@@ -1,28 +1,52 @@
-int command = 0;
-int state = 0;
+// Biblioteke
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include "DHT.h"
+
+#define DHTPIN 10     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE DHT11   // DHT 22  (AM2302), AM2321
+
+DHT dht(DHTPIN, DHTTYPE);
+char command;
+float t;
+float v;
+
 void setup() {
-  // relej na izlaznom pinu.
   Serial.begin(9600);
-  pinMode(10, OUTPUT);
+  Serial.println("DHT11 test!");
+  dht.begin();
 }
 
 void loop() {
-  if(Serial.available()>0){
-  command = Serial.read();
-  Serial.println(command);
-  
-    if(command == 49 && state==0)
-        Serial.println(1);
-        state = 1;
-        digitalWrite(10, state);
-        while(Serial.read()!=48)
-        {}
-                
-    if(command == 48 & state==1)
-        Serial.println(0);
-        state = 0;
-        digitalWrite(10, state);    
-        while(Serial.read()!=49)
-        {}  
+
+  if(Serial.available()){
+    command = Serial.read();
+    Serial.println(command);
+    if(command=='t'){
+      t = dht.readTemperature();
+      if(isnan(t))
+      Serial.println("Greska prilikom citanja temperature!");
+      else{
+        Serial.println("Temperatura:");
+        Serial.print(t);
+        Serial.print("°C "); 
+        delay(1000);
+     }
+    }
+    if(command=='v'){
+      v = dht.readHumidity();
+      if(isnan(v))
+      Serial.println("Greska prilikom citanja vlaznosti!");
+      else{ 
+        Serial.println("Vlaznost:");
+        Serial.print(v);
+        Serial.print("%"); 
+        delay(1000);
+     }
+    }
+ 
   }
+
 }
