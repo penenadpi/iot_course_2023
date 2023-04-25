2b235ce15d0b01668646a5f5c87725a88dd427c4
penenadpi <penenadpi@gmail.com>
2023-04-25 20:29:40 +0200

    Add files via upload

---
+++ live_arduino_test_1.ino
@@ -0,0 +1,55 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include "DHT.h"
+
+#define DHTPIN 10     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE DHT11   // DHT 22  (AM2302), AM2321
+
+DHT dht(DHTPIN, DHTTYPE);
+char command;
+float t;
+float v;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 test!");
+  dht.begin();
+}
+
+void loop() {
+
+  if(Serial.available()){
+    command = Serial.read();
+    Serial.println(command);
+    if(command=='t'){
+      t = dht.readTemperature();
+      if(isnan(t))
+      Serial.println("Greska prilikom citanja temperature!");
+      else{
+        Serial.println("Temperatura:");
+        Serial.print(t);
+        Serial.print("°C "); 
+        delay(1000);
+     }
+    }
+    if(command=='v'){
+      v = dht.readHumidity();
+      if(isnan(v))
+      Serial.println("Greska prilikom citanja vlaznosti!");
+      else{ 
+        Serial.println("Vlaznost:");
+        Serial.print(v);
+        Serial.print("%"); 
+        delay(1000);
+     }
+    }
+ 
+  }
+
+}
