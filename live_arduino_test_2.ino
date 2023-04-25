fb76921d830b259d5eeb201adf90d8b09481a6f3
penenadpi <penenadpi@gmail.com>
2023-04-25 20:29:50 +0200

    Add files via upload

---
+++ live_arduino_test_2.ino
@@ -0,0 +1,28 @@
+int command = 0;
+int state = 0;
+void setup() {
+  // relej na izlaznom pinu.
+  Serial.begin(9600);
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  if(Serial.available()>0){
+  command = Serial.read();
+  Serial.println(command);
+  
+    if(command == 49 && state==0)
+        Serial.println(1);
+        state = 1;
+        digitalWrite(10, state);
+        while(Serial.read()!=48)
+        {}
+                
+    if(command == 48 & state==1)
+        Serial.println(0);
+        state = 0;
+        digitalWrite(10, state);    
+        while(Serial.read()!=49)
+        {}  
+  }
+}
