a367816876000ef125da73b5c0bcd26caeaa9769
penenadpi <penenadpi@gmail.com>
2024-04-24 00:02:16 +0200

    Add files via upload

---
+++ r4_wifi_server_2024.ino
@@ -0,0 +1,109 @@
+
+
+#include "WiFiS3.h"
+
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = "naziv_mreze";        // your network SSID (name)
+char pass[] = "sifra";    // your network password (use for WPA, or use as key for WEP)
+
+int status = WL_IDLE_STATUS;
+char c;
+WiFiServer server(80);
+void printWifiStatus();
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  server.begin();
+  // you're connected now, so print out the status:
+  printWifiStatus();
+}
+
+
+void loop() {
+  // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an HTTP request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        c = client.read();
+        Serial.write(c);
+        //Novi red oznacava kraj HTTP zahteva
+        if (c == '\n' && currentLineIsBlank) {
+          // Saljemo standardni HTTP header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // konekcija se zatvara po odgovoru
+          client.println("Refresh: 5");  // refresh strane na 5 sekundi
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+
+          int sensorReading = random(35);
+          client.print("Sensor value is ");
+
+          client.print(sensorReading);
+          client.print("<br />");
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+
+    // close the connection:
+    client.stop();
+    //Serial.println("client disconnected");
+  }
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
