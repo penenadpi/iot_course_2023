413fb30e9fde70d8ece27f8468da594e20ececcb
penenadpi <penenadpi@gmail.com>
2024-05-08 12:54:16 +0200

    Add files via upload

---
+++ uno_r4_wifi_mqtt.ino
@@ -0,0 +1,192 @@
+#include <WiFiS3.h>
+#include <ArduinoMqttClient.h>
+
+char ssid[] = "YOUR_SSID";        
+char pass[] = "YOUR_PASSWORD";    
+int status = WL_IDLE_STATUS;    
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "broker.hivemq.com";
+int        port     = 1883;
+const char topic[]  = "topic_receive";
+const char topic_send[]  = "topic_send";
+
+const long interval = 1000;
+unsigned long previousMillis = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+
+  // You can provide a unique client ID, if not set the library uses Arduino-millis()
+  // Each client must have a unique client ID
+  // mqttClient.setId("clientId");
+
+  // You can provide a username and password for authentication
+  // mqttClient.setUsernamePassword("username", "password");
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+
+  // set the message receive callback
+  mqttClient.onMessage(onMqttMessage);
+
+  Serial.print("Subscribing to topic: ");
+  Serial.println(topic);
+  Serial.println();
+
+  // subscribe to a topic
+  mqttClient.subscribe(topic);
+
+  // topics can be unsubscribed using:
+  // mqttClient.unsubscribe(topic);
+
+  Serial.print("Waiting for messages on topic: ");
+  Serial.println(topic);
+  Serial.println();
+  
+  int value = random(30);
+  mqttClient.beginMessage(topic_send);
+  mqttClient.println(value); 
+  mqttClient.endMessage(); 
+
+
+}
+
+void loop() {
+
+  // call poll() regularly to allow the library to receive MQTT messages and
+  // send MQTT keep alives which avoids being disconnected by the broker
+  mqttClient.poll();
+
+  // Add a delay to avoid constant pinging
+  delay(5000);
+}
+
+void printWifiData() {
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  printMacAddress(mac);
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  printMacAddress(bssid);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
+void printMacAddress(byte mac[]) {
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
+}
+
+
+void onMqttMessage(int messageSize) {
+  // we received a message, print out the topic and contents
+  Serial.print("Received a message with topic '");
+  Serial.print(mqttClient.messageTopic());
+  Serial.println("'");
+  Serial.print("Message length: ");
+  Serial.print(messageSize);
+  Serial.println(" bytes: ");
+
+  // Read the message contents into a String
+  String message = mqttClient.readString();
+
+  // Convert the received message and comparison strings to lowercase
+  message.toLowerCase();
+
+  // Check if the contents of the message match for solve or reset
+  if(message == "whatYouWantToSee") {
+    onCustom();
+    Serial.println("Custom Message For Debugging");
+  } else if (message == "whatYouWantToSee2") {
+    onCustom2();
+    Serial.println("Custom Message For Debugging2");
+  } else {
+    // If it doesn't match either of the key words, print the message contents for debugging
+  Serial.print("Message: ");
+  Serial.println(message);
+  }
+  Serial.println();
+
+}
+
+void onCustom(){
+  //Code you want to run when message 1 gets here
+}
+
+void onCustom2(){
+  //Code you want to run when message 1 gets here
+}
