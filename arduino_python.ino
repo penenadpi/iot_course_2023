53aed78fec97c1648f66c92ee2f68a2b8b20ebab
penenadpi <penenadpi@gmail.com>
2024-05-20 09:09:53 +0200

    Add files via upload

---
+++ arduino_python.ino
@@ -0,0 +1,10 @@
+void setup() {
+  Serial.begin(9600);  // Start serial communication at 9600 baud rate
+}
+
+void loop() {
+  int sensorValue = random(35);  // Generate temperature value
+  Serial.println(sensorValue);       // Send the value to the serial port
+  delay(10000);                       // Wait for 1 second
+}
+  
